fa08a99ba7b8f5406e1f3a031bc3e4180b5df6d1
bornfromapp <habaek2002@gmail.com>
2024-12-03 20:54:17 +0700

    m

---
+++ WebApplication1/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HienThiDSSP()
+        {
+            ConnectSanPham obj = new ConnectSanPham();
+            List<SanPham> list = obj.getData();
+            return View(list);
+        }
+    }
+}
---
+++ WebApplication1/Models/ConnectSanPham.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web;
+using System.Configuration;
+
+namespace WebApplication1.Models
+{
+    public class ConnectSanPham
+    {
+        public string constr = ConfigurationManager.ConnectionStrings["Connect1"].ConnectionString;
+        public List<SanPham> getData()
+        {
+            List<SanPham> lsSP = new List<SanPham>();
+            SqlConnection con = new SqlConnection(constr);
+            string sql = "select * from SanPham";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                SanPham sp = new SanPham();
+                sp.MaSP = Convert.ToInt32(rdr.GetValue(0).ToString());
+                sp.TenSP = rdr.GetValue(1).ToString();
+                sp.DuongDan = rdr.GetValue(2).ToString();
+                sp.Gia = float.Parse(rdr.GetValue(3).ToString());
+                sp.MoTa = rdr.GetValue(4).ToString();
+                sp.MaLoai = Convert.ToInt32(rdr.GetValue(5).ToString());
+                lsSP.Add(sp);
+            }
+            return lsSP;
+        }
+    }
+}
